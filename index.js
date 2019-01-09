42a9ad3ec8a5af8e60b179bd6158eae208f03fa1
Doug Fitzgerald <dfitzge1@conncoll.edu>
2019-01-09 15:58:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(drivers, driverName) {
+  // debugger;
+  return drivers.filter(function(name) {return name.toLowerCase() == driverName.toLowerCase()})
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(function(name) {return name.slice(0, letters.length) == letters})
+}
+
+function matchName(drivers, letters) {
+  for (const key in drivers){
+    // debugger;
+    return drivers.filter(function(name) {return name["name"].slice(0, letters.length) == letters})
+  }
+}
